59ae2da66d663e22bc817a355e772f77f956f7b0
nisayaman <137568642+nisayaman@users.noreply.github.com>
2024-05-04 16:01:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const quizData = [
-    {
-      question: "Evet hayır oyunu oynayacağız. Sana 2 soru soracağım ve sadece bir kere evet ya da hayır deme hakkın var. Hazır mısın?",
-      options: ["Evet", "Hayır", "Emin Değilim"],
-      answer: "Evet"
-    },
-    {
-      question: "Gay misin?",
-      options: ["Evet", "Hayır"],
-      answer: ["Evet", "Hayır"]
-    },
-  {
-      question: "Yalan mı söylüyorsun?",
-      options: ["Evet", "Hayır"],
-      answer: ["Evet", "Hayır"]
-    },
-  ];
-  
+
+ def question_test():
+    # Let's store the questions and answers in a dictionary
+    questions = {
+        "1. Question": "When was Python developed?",
+        "2. Question": "Who is the creator of Python?",
+        "3. Question": "What is the result of adding 2 and 3 in Python?"
+    }
+
+    answers = {
+        "1. Question": "1991",
+        "2. Question": "Guido van Rossum",
+        "3. Question": "5"
+    }
   
   showQuestion();
